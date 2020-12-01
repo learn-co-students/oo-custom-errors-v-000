f5cbf36fe7b7a388d2a950245f122f7cf58ba758
andy-aguilar <aaguil3@gmail.com>
2020-12-01 09:06:50 -0600

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,10 +7,24 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    else
+      person.partner = self
+    end
   end
-
+  
+  class PartnerError < StandardError
+    def message
+      "Jay-Z isn't good enough for Beyonce!"
+    end
+  end
 end
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
